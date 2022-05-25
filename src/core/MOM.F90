d30ebbdd8210cc25274622af4ba004f1a0189d23
Andrew C. Ross <andrew.c.ross@noaa.gov>
2022-05-25 13:33:30 -0400

    Missing OBC in call_tracer_register

--- src/core/MOM.F90
+++ src/core/MOM.F90
@@ -2443,7 +2443,7 @@ subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
   ! This subroutine calls user-specified tracer registration routines.
   ! Additional calls can be added to MOM_tracer_flow_control.F90.
   call call_tracer_register(HI, GV, US, param_file, CS%tracer_flow_CSp, &
-                            CS%tracer_Reg, restart_CSp)
+                            CS%tracer_Reg, restart_CSp, CS%OBC)
 
   call MEKE_alloc_register_restart(HI, US, param_file, CS%MEKE, restart_CSp)
   call set_visc_register_restarts(HI, GV, US, param_file, CS%visc, restart_CSp)
