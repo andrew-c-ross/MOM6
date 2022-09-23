cf60b7458e65d27d0bbf8064cbba50966a8edb20
Andrew C. Ross <Andrew.C.Ross@noaa.gov>
2022-09-23 10:33:50 -0400

    Zero out STF and only use rivers

--- src/tracer/MOM_generic_tracer.F90
+++ src/tracer/MOM_generic_tracer.F90
@@ -521,10 +521,11 @@ contains
         call g_tracer_get_pointer(g_tracer,g_tracer_name,'trunoff',trunoff_array)
         call g_tracer_get_pointer(g_tracer,g_tracer_name,'runoff_tracer_flux',runoff_tracer_flux_array)
         !nnz: Why is fluxes%river = 0?
-        runoff_tracer_flux_array(:,:) = trunoff_array(:,:) * &
-                 US%RZ_T_to_kg_m2s*fluxes%lrunoff(:,:)
+        !runoff_tracer_flux_array(:,:) = trunoff_array(:,:) * &
+        !         US%RZ_T_to_kg_m2s*fluxes%lrunoff(:,:)
         do j = jsc, jec ; do i = isc, iec
-          all_stf(i,j) = ((trunoff_array(i,j) * US%RZ_T_to_kg_m2s * fluxes%lrunoff(i,j)) + stf_array(i,j)) * (dt * GV%RZ_to_H) 
+          !all_stf(i,j) = ((trunoff_array(i,j) * US%RZ_T_to_kg_m2s * fluxes%lrunoff(i,j)) + stf_array(i,j)) * (dt * GV%RZ_to_H) 
+          all_stf(i,j) = ((trunoff_array(i,j) * US%RZ_T_to_kg_m2s * fluxes%lrunoff(i,j))) 
           stf_array(i,j) = 0.0
         enddo; enddo 
         ! stf_array = stf_array + runoff_tracer_flux_array
